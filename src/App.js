f4115814484c18779684e53b1cf719497502aba6
IchadB <richardbetalmos@gmail.com>
2023-06-15 14:34:09 +0800

    updating app.js

--- src/App.js
+++ src/App.js
@@ -40,26 +40,6 @@ function App() {
           element={<CreateExamPt2 />}
         />
         <Route path="*" element={<NotFound />} />
-	// const url = 'http://localhost:5000/teachers/get-students';
-	// fetch(url).then(res => res.json).then(data=> console.log(data));
-
-	return (
-		<>
-			<Routes>
-				<Route path='/' element={<LandingPage />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/register' element={<Register />} />
-				<Route path='/teacher' element={<Home />} />
-				<Route path='/teacher/students' element={<Students />} />
-				<Route path='/teacher/grades' element={<Grades />} />
-				<Route path='/teacher/profile' element={<TeacherProfile />} />
-				<Route path='/teacher/activities' element={<Activities />} />
-				<Route path='/teacher/activities/:id' element={<ActivityDetails />} />
-				<Route
-					path='/teacher/activities/part2/:id'
-					element={<CreateExamPt2 />}
-				/>
-				<Route path='*' element={<NotFound />} />
 
         {/* Students Page */}
         <Route path="/student" element={<StudentsPage />} />
