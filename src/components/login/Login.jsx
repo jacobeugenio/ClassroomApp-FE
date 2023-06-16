4e85cbe0aa2a974bc1e26407f49a792ad825498a
IchadB <richardbetalmos@gmail.com>
2023-06-16 13:57:48 +0800

    added redux and logout config for data persisting and deletion

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,43 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import "./login.css";
 
-import API_Service from "../../api-service/API_Service";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { login } from "../../redux/user";
+import { useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../../redux/usersApiSlice";
+import { setCredentials } from "../../redux/authSlice";
 
 const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
   const [userLogin, setUserLogin] = useState({
     username: "",
     password: "",
   });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+  const { userInfo } = useSelector((state) => state.auth);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    // console.log(name, value);
+    console.log(name, value);
     setUserLogin((prevFormData) => ({ ...prevFormData, [name]: value }));
   };
 
   const handleLoginSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await API_Service.post("/users/login", userLogin);
-      console.log(typeof response.data.id);
-      if (!response.data.status) {
-        console.log("User not found");
+      const res = await login(userLogin);
+      console.log(res);
+      if (res.data.status) {
+        if (res.data.type === "teacher") {
+          dispatch(setCredentials({ ...res }));
+          navigate("/teacher");
+        } else {
+          dispatch(setCredentials({ ...res }));
+          navigate("/student");
+        }
       } else {
-        response.data.type === "student"
-          ? navigate("/students")
-          : navigate("/teacher");
-        dispatch(login({ userID: response.data.id }));
+        console.log("Invalid Credentials");
       }
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      console.log(err?.data?.message || err.error);
     }
   };
 
--- src/components/students/layout/Header/Header-Students.jsx
+++ src/components/students/layout/Header/Header-Students.jsx
@@ -8,31 +8,48 @@ import Navbar from "react-bootstrap/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import Button from "react-bootstrap/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLogoutMutation } from "../../../../redux/usersApiSlice";
+import { logout } from "../../../../redux/authSlice";
 
 function HeaderStudents() {
-	return (
-		<div>
-			<Navbar bg='light' expand='lg' className='navbar'>
-				<Container>
-					<Navbar.Brand href='#home'>Classroom App</Navbar.Brand>
-					<Navbar.Toggle aria-controls='basic-navbar-nav' />
-					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='ms-auto'>
-							<Nav.Link href='/student'>Home</Nav.Link>
-							<Nav.Link href='/student/attendance'>Attendance</Nav.Link>
-							<Nav.Link href='/student/grades'>Grades</Nav.Link>
-							<Nav.Link href='/student/exams'>Exams</Nav.Link>
-							<Nav.Link to='/student/logout'>
-								<Button variant='warning' size='sm'>
-									<FontAwesomeIcon icon={faRightFromBracket} /> LOGOUT
-								</Button>
-							</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-		</div>
-	);
+  const { userInfo } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [logoutApiCall] = useLogoutMutation();
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      <Navbar bg="light" expand="lg" className="navbar">
+        <Container>
+          <Navbar.Brand href="#home">Classroom App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/student">Home</Nav.Link>
+              <Nav.Link href="/student/attendance">Attendance</Nav.Link>
+              <Nav.Link href="/student/grades">Grades</Nav.Link>
+              <Nav.Link href="/student/exams">Exams</Nav.Link>
+              <Nav.Link to="/student/logout">
+                <Button variant="warning" size="sm" onClick={logoutHandler}>
+                  <FontAwesomeIcon icon={faRightFromBracket} /> LOGOUT
+                </Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default HeaderStudents;
