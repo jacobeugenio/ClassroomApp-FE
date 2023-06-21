0e1b7e1b2767e73ef1a1b16a4c625f8ad74d65dd
IchadB <richardbetalmos@gmail.com>
2023-06-21 20:44:12 +0800

    updating

--- src/components/students/components/Exams/Exams-Cards-Students.jsx
+++ src/components/students/components/Exams/Exams-Cards-Students.jsx
@@ -14,75 +14,75 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHandPointer } from "@fortawesome/free-solid-svg-icons";
 
 function ExamsCards() {
-	const [exams, setExams] = useState([]);
+  const [exams, setExams] = useState([]);
 
-	const { userInfo } = useSelector((state) => state.auth);
+  const { userInfo } = useSelector((state) => state.auth);
 
-	const getExams = async () => {
-		try {
-			const response = await API_Service.get("/teachers/activities", {
-				headers: {
-					Authorization: `Bearer ${userInfo.data.token}`,
-				},
-			});
-			console.log(response.data);
-			setExams(response.data);
-		} catch (error) {
-			console.error(error);
-		}
-	};
-	useEffect(() => {
-		getExams();
-	}, []);
+  const getExams = async () => {
+    try {
+      const response = await API_Service.get("/students/exams", {
+        headers: {
+          Authorization: `Bearer ${userInfo.data.token}`,
+        },
+      });
+      console.log(response.data);
+      setExams(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getExams();
+  }, []);
 
-	return (
-		<div className='body__card'>
-			<HeaderStudents />
-			<Container className='exam__card--container'>
-				<Row>
-					{exams &&
-						exams.map((exam) => {
-							return (
-								<Col lg={3} md={4} sm={4} className='my-3' key={exam._id}>
-									<Card className='card__exam'>
-										<Card.Header className='d-flex justify-content-between'>
-											<div className='exam__subject'>{exam.subject}</div>
-											<div className='exam__length'>{exam.examLength}</div>
-										</Card.Header>
-										<Card.Body>
-											<Card.Title>
-												<div className='exam__title'>{exam.title}</div>
-											</Card.Title>
-										</Card.Body>
-										<Card.Footer
-											className='text-muted d-flex'
-											style={{ fontSize: 10 }}
-										>
-											<Link
-												className='text-decoration-none  btn__exam'
-												to={`/student/take-exams/` + exam._id}
-											>
-												<Button
-													variant='outline-success'
-													size='sm'
-													className='btn__exam fw-bold'
-												>
-													<FontAwesomeIcon
-														icon={faHandPointer}
-														className='me-1'
-													/>
-													Open
-												</Button>
-											</Link>
-										</Card.Footer>
-									</Card>
-								</Col>
-							);
-						})}
-				</Row>
-			</Container>
-		</div>
-	);
+  return (
+    <div className="body__card">
+      <HeaderStudents />
+      <Container className="exam__card--container">
+        <Row>
+          {exams &&
+            exams.map((exam) => {
+              return (
+                <Col lg={3} md={4} sm={4} className="my-3" key={exam._id}>
+                  <Card className="card__exam">
+                    <Card.Header className="d-flex justify-content-between">
+                      <div className="exam__subject">{exam.subject}</div>
+                      <div className="exam__length">{exam.examLength}</div>
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Title>
+                        <div className="exam__title">{exam.title}</div>
+                      </Card.Title>
+                    </Card.Body>
+                    <Card.Footer
+                      className="text-muted d-flex"
+                      style={{ fontSize: 10 }}
+                    >
+                      <Link
+                        className="text-decoration-none  btn__exam"
+                        to={`/student/take-exams/` + exam._id}
+                      >
+                        <Button
+                          variant="outline-success"
+                          size="sm"
+                          className="btn__exam fw-bold"
+                        >
+                          <FontAwesomeIcon
+                            icon={faHandPointer}
+                            className="me-1"
+                          />
+                          Open
+                        </Button>
+                      </Link>
+                    </Card.Footer>
+                  </Card>
+                </Col>
+              );
+            })}
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default ExamsCards;
