d3132a1798fcc849d8af6cf4e13023bfc5bb19eb
IchadB <richardbetalmos@gmail.com>
2023-06-22 21:14:46 +0800

    updating

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -2,6 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://classroom-app-be.vercel.app",
+  prepareHeaders: (headers) => {
+    headers.set("Content-Type", "application/json");
+    // headers.set("Authorization", "Bearer your_token_here");
+    headers.set("Access-Control-Allow-Credentials", "true");
+    return headers;
+  },
 });
 
 export const apiSlice = createApi({
