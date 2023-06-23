7f41916611ee9ab699a2916bf837b6b64e3e0267
IchadB <richardbetalmos@gmail.com>
2023-06-23 13:32:32 +0800

    solutuin 7

--- src/redux/usersApiSlice.js
+++ src/redux/usersApiSlice.js
@@ -1,28 +1,32 @@
 import { apiSlice } from "./apiSlice";
 export const usersApiSlice = apiSlice.injectEndpoints({
-	endpoints: (builder) => ({
-		register: builder.mutation({
-			query: (data) => ({
-				url: "/users/register",
-				method: "POST",
-				body: data,
-			}),
-		}),
-		login: builder.mutation({
-			query: (data) => ({
-				url: "/users/login",
-				method: "POST",
-				body: data,
-			}),
-		}),
-		logout: builder.mutation({
-			query: () => ({
-				url: "/users/logout",
-				method: "POST",
-			}),
-		}),
-	}),
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "/users/register",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/users/login",
+        method: "POST",
+        body: data,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "/users/logout",
+        method: "POST",
+      }),
+    }),
+  }),
 });
 
 export const { useRegisterMutation, useLoginMutation, useLogoutMutation } =
-	usersApiSlice;
+  usersApiSlice;
