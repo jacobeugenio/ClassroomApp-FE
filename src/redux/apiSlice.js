3c0a596e39797c3d836692038c7be831e296905f
IchadB <richardbetalmos@gmail.com>
2023-06-23 10:43:50 +0800

    updating

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://classroom-app-be.vercel.app",
+  baseUrl: "http://localhost:5000",
 });
 
 export const apiSlice = createApi({
